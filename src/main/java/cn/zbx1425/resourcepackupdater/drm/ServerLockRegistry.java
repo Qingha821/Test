cd3bc4a86b9a8e9e7db44a36b89152a7989464d9
qha821 <qingha821@outlook.com>
2025-02-08 10:20:35 +0800

    Fix Errors

--- src/main/java/cn/zbx1425/resourcepackupdater/drm/ServerLockRegistry.java
+++ src/main/java/cn/zbx1425/resourcepackupdater/drm/ServerLockRegistry.java
@@ -7,7 +7,6 @@ import com.google.gson.JsonParser;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.toasts.SystemToast;
 import org.apache.commons.io.IOUtils;
-import net.minecraft.client.gui.components.toasts.SystemToastIds;
 
 
 import java.io.File;
@@ -69,10 +68,10 @@ public class ServerLockRegistry {
 
     public static void onAfterSetServerLock() {
         if (lockAllSyncedPacks) {
-            Minecraft.getInstance().getToasts().addToast(new SystemToast(SystemToastIds.WORLD_ACCESS_FAILURE,
+            Minecraft.getInstance().getToasts().addToast(new SystemToast(SystemToast.Type.PACK_LOAD_FAILURE,
                     Text.literal("同步資源包不完整而未被采用"), Text.literal("您可按 F3+T 重試下載。如有錯誤請聯絡管理人員。")
-            ));
-            Minecraft.getInstance().getToasts().addToast(new SystemToast(SystemToastIds.WORLD_ACCESS_FAILURE,
+            ))
+            Minecraft.getInstance().getToasts().addToast(new SystemToast(SystemToast.Type.PACK_LOAD_FAILURE,
                     Text.literal("Synced Resource Pack Incomplete and Thus not Used"), Text.literal("Press F3+T to download again. Ask the staff when error.")
             ));
         }
